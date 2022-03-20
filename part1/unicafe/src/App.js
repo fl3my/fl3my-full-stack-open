44734983484932a387d77831fa2488fadd4097e5
Ross Fleming <rossflem@icloud.com>
2022-03-20 11:23:09 +0000

    completed up to 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,17 +5,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setAll(all + 1)
   }
   
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setAll(all + 1)
   }
 
   return (
@@ -25,9 +29,7 @@ const App = () => {
       <Button onClick={handleNeutralClick} text='neutral' />
       <Button onClick={handleBadClick} text='bad' />
       <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
     </div>
   )
 }
@@ -40,4 +42,37 @@ const Button = (props) => {
   )
 }
 
+const Statistics = ({good, neutral, bad, all}) => {
+
+  if (all === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  const average = (good + (bad * -1)) / all;
+  const positive = (good / all) * 100;
+
+  return(
+    <table>
+      <tbody>
+        <StatisticsLine text="good" value={good} />
+        <StatisticsLine text="neutral" value={neutral} />
+        <StatisticsLine text="bad" value={bad} />
+        <StatisticsLine text="all" value={all} />
+        <StatisticsLine text="average" value={average} />
+        <StatisticsLine text="positive" value={positive} unit="%"/>
+      </tbody>
+    </table>
+  )
+}
+
+const StatisticsLine = (props) => {
+  return(
+    <tr>
+      <td>{props.text}</td>
+        <td>{props.value}{props.unit}</td>
+    </tr>
+  )
+}
 export default App
